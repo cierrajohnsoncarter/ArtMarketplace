796477938d7bf5786ed69c93136f88b3cc566c2b
cierrajohnsoncarter <cierrajohnsoncarter@outlook.com>
2020-04-19 12:44:14 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+#**The following illustrates the funtionality of the various classes. Feel free tp uncomment the calls/print statements and run it in the code to test it out
+
+#**Client instantiation - adds a client
+#edytta = Client('Edytta Halpirt', None, False)
+#moma = Client('The MOMA', 'New York', True)
+
+#**Art instantiation - adds artwork
+#girl_with_mandolin = Art('Picasso, Pablo', "Girl with a Mandolin (Fanny Tellier)", 1910, 'oil on canvas', edytta)
+#print(girl_with_mandolin)
+
+#**Marketplace instantiaion - shows listings
+#veneer = Marketplace()
+#print(veneer.show_listing())
+
+#**Creates a new listing
+#edytta.sell_artwork(girl_with_mandolin, '6M (USD)')
+#veneer.show_listing()
+
+#**Allows MOMA to buy artwork
+#moma.buy_artwork(girl_with_mandolin)
+
+#**After MOMA purchases the artwork, no listings should be left
+#veneer.show_listing()
